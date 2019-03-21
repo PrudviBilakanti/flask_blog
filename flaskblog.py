d365e8e8d1151a25f0fface31b5dd79dca57fe21
Prudvi Bilakanti <pavan.bilakanti@gmail.com>
2019-03-21 23:44:44 +0530

    Adding loops and conditions in templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'Author':'Prudvi Bilakanti',
+        'Content':'End to end machine learning flow',
+        'Posted':'3/21/2019'
+    },
+{
+        'Author':'Rudra Shrihaan',
+        'Content':'End to end machine learning flow',
+        'Posted':'3/22/2019'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
